e623f9779f111b145f697dee7db52b4da16cd6ec
Thorsten Deinert <thorsten.deinert@udo.edu>
2026-02-27 17:26:03 +0100

    1.2 Move greeting into its own class

--- HelloPostSharp/Program.cs
+++ HelloPostSharp/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var myObject = new MyClass();
+myObject.MyMethod();
+
+
+
+class MyClass
+{
+    public void MyMethod()
+    {
+        Console.WriteLine("Hello from MyMethod!");
+    }
+}
