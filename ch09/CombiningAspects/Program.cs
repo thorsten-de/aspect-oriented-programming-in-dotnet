02ca198be356b8a8081d480d478c1b74f800cc67
Thorsten Deinert <thorsten.deinert@udo.edu>
2026-03-28 19:31:52 +0100

    9.4 Try/Catch for authorization errors

--- ch09/CombiningAspects/Program.cs
+++ ch09/CombiningAspects/Program.cs
@@ -10,15 +10,22 @@ var container = new Container(x =>
     });
 });
 
-string accountNumber = "0815";
-var budgetService = container.GetInstance<IBudgetService>();
+try
+{
+    string accountNumber = "0815";
+    var budgetService = container.GetInstance<IBudgetService>();
 
-decimal budget = budgetService.GetBudgetForAccount(accountNumber);
-Console.WriteLine($"Account {accountNumber} has budget {budget:C}");
+    decimal budget = budgetService.GetBudgetForAccount(accountNumber);
+    Console.WriteLine($"Account {accountNumber} has budget {budget:C}");
 
-// If caching works, we get the same budget, not a random new one
-var budgetAgain = budgetService.GetBudgetForAccount(accountNumber);
-Console.WriteLine($"Account {accountNumber} has budget {budgetAgain:C}");
+    // If caching works, we get the same budget, not a random new one
+    var budgetAgain = budgetService.GetBudgetForAccount(accountNumber);
+    Console.WriteLine($"Account {accountNumber} has budget {budgetAgain:C}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Unable to retrieve budget. Error: {ex.Message}");
+}
 
 
 
